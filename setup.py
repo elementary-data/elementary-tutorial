ccc7d29f7e4cf35b96da67134464e212fa18bc4d
Elon Gliksberg <elongliks@gmail.com>
2022-10-31 12:07:15 +0200

    Updated the requirements.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup, find_packages
 import pathlib
+
+from setuptools import setup, find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -19,35 +20,14 @@ setup(
         elementary-tutorial=elementary_tutorial.cli:cli
     """,
     long_description=README,
-    install_requires=["click>=7.0,<9", "dbt-core>=0.20,<2.0.0", "elementary-data"],
+    install_requires=["click>=7.0,<9", "elementary-data"],
     extras_require={
-        "snowflake": ["dbt-snowflake>=0.20,<2.0.0"],
-        "bigquery": ["dbt-bigquery>=0.20,<2.0.0"],
-        "redshift": ["dbt-redshift>=0.20,<2.0.0"],
-        "postgres": ["dbt-postgres>=0.20,<2.0.0"],
-        "lineage": [
-            "snowflake-connector-python[secure-local-storage]>=2.4.1,<2.8.0",
-            "click>=8,<9",
-            "requests>=2.7,<3.0.0",
-            "networkx>=2.3,<3",
-            "sqllineage==1.2.4",
-            "pyvis>=0.1,<1",
-            "sqlparse>=0.3.1,<0.5",
-            "sqlfluff<0.9.0",
-            "google-cloud-bigquery>=1.25.0,<3",
-            "google-api-core>=1.16.0,<3",
-            "python-dateutil>=2.7.2,<3.0.0",
-            # temp solution until refactor of utils.dbt
-            "dbt-bigquery>=0.20,<2.0.0",
-            # Indirect
-            "protobuf<4,>=3.13.0",
-            "typing-extensions<3.11,>=3.7.4",
-            "flask<1.1.1",
-            "Jinja2>=2.10.1,<3",
-            "tomli<2.0.0,>=0.2.6",
-            "urllib3<1.26,>=1.20",
-            "MarkupSafe==2.0.1",
-        ],
+        "snowflake": ["elementary-data[snowflake]"],
+        "bigquery": ["elementary-data[bigquery]"],
+        "redshift": ["elementary-data[redshift]"],
+        "postgres": ["elementary-data[postgres]"],
+        "databricks": ["elementary-data[databricks]"],
+        "spark": ["elementary-data[spark]"],
     },
     long_description_content_type="text/markdown",
     url="https://github.com/elementary-data/tutorial",
