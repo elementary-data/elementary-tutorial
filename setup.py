8ec79a5c91ee650a9edcaaf1ac9a95db5be82de4
Noy Arie <noyarie1992@gmail.com>
2025-04-21 16:54:22 +0300

    update python version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="Tutorial for Elementary.",
     version="0.1.0",
     packages=find_packages(),
-    python_requires=">=3.6.2",
+    python_requires=">=3.8",
     author="Elementary",
     entry_points="""
         [console_scripts]
