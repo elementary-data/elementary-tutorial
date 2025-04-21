c24c6a26e17e0a55baf831a52ea6a3f5655a8539
Noy Arie <noyarie1992@gmail.com>
2025-04-21 17:51:13 +0300

    remove pyfiglet package

--- elementary_tutorial/cli.py
+++ elementary_tutorial/cli.py
@@ -1,11 +1,22 @@
 import click
-from pyfiglet import Figlet
 
 from elementary_tutorial.workflows.generate_data import generate_data
 
 
-f = Figlet(font="slant")
-click.echo(f.renderText("Elementary Tutorial"))
+ELEMENTARY_LOGO = r"""
+    ________                          __                  
+   / ____/ /__  ____ ___  ___  ____  / /_____ ________  __
+  / __/ / / _ \/ __ `__ \/ _ \/ __ \/ __/ __ `/ ___/ / / /
+ / /___/ /  __/ / / / / /  __/ / / / /_/ /_/ / /  / /_/ / 
+/_____/_/\___/_/ /_/ /_/\___/_/ /_/\__/\__,_/_/   \__, /  
+                                                 /____/   
+  ______      __             _       __
+ /_  __/_  __/ /_____  _____(_)___ _/ /
+  / / / / / / __/ __ \/ ___/ / __ `/ / 
+ / / / /_/ / /_/ /_/ / /  / / /_/ / /  
+/_/  \__,_/\__/\____/_/  /_/\__,_/_/   
+"""
+click.echo(f"{ELEMENTARY_LOGO}\n")
 
 
 @click.group
